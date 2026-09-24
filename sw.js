f16ebf2915ec77969cfc292dba92715bbfa2d927
rafaeldatsch4-afk <rafaeldatsch4@gmail.com>
2026-09-24 12:51:12 -0300

    Ativa instalação PWA e cache do aplicativo

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE='bom-regresso-pwa-v1';
+const SHELL=['/','/index.html','/manifest.webmanifest','/app-icon.svg'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  const url=new URL(req.url);
+  if(url.origin!==self.location.origin) return;
+
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(cache=>cache.put('/index.html',copy));
+        return res;
+      }).catch(()=>caches.match('/index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+      if(res && res.ok){
+        const copy=res.clone();
+        caches.open(CACHE).then(cache=>cache.put(req,copy));
+      }
+      return res;
+    }))
+  );
+});
